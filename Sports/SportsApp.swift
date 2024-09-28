054678fc3b2bc54857e23d8afb38f6b116adf9d3
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2024-09-28 18:29:06 +0300

    Initial Commit

---
+++ Sports/SportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsApp.swift
+//  Sports
+//
+//  Created by Mohammed Skaik on 28/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
