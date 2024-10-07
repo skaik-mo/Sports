2c084e8b8a20ce20b8c8e085383896f2f2460d8a
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2024-10-07 17:37:10 +0300

    Set Alert & Progress View In SportsApp

--- Sports/SportsApp.swift
+++ Sports/SportsApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct SportsApp: App {
+    @State var alert = AlertManager()
+    @State var progress = ProgressManager()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.alertKey, alert)
+                .environment(\.progressKey, progress)
+                .errorAlert($alert)
+//                .sheet(isPresented: $progress.showProgress) {
+            .overlay {
+                if progress.showProgress {
+                    CustomProgressView()
+                }
+            }
         }
     }
 }
