e46c967b78b1c6e1bc9611d0c2bc9e5014416613
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2024-10-08 17:17:02 +0300

    Update Sports

--- Sports/Models/Sports.swift
+++ Sports/Models/Sports.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-enum Sports: CaseIterable, Identifiable {
+enum Sports: String, CaseIterable, Identifiable {
     var id: Self { self }
     case football
     case cricket
@@ -36,6 +36,23 @@ enum Sports: CaseIterable, Identifiable {
         case .tennis:
             return .tennis
         }
+    }
+
+    var icon: String {
+        switch self {
+        case .football:
+            return "soccerball.inverse"
+        case .basketball:
+            return "basketball.fill"
+        case .cricket:
+            return "cricket.ball.fill"
+        case .tennis:
+            return "tennisball.fill"
+        }
+    }
+
+    var endPoint: String {
+        return "\(APIConstants.API.rawValue)\(self.rawValue)"
     }
 
 }
