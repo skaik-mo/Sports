50cd56579e04c6849f15cc7fc7e94e5d0973baf5
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2024-10-08 17:16:37 +0300

    Update LeaguesView & LeaguesViewModel

--- Sports/Views/Scenes/Leagues/View/LeaguesView.swift
+++ Sports/Views/Scenes/Leagues/View/LeaguesView.swift
@@ -9,41 +9,62 @@ import SwiftUI
 
 struct LeaguesView: View {
 
-    @State var viewModel: LeaguesViewModel
+    @State private var viewModel: LeaguesViewModel
+    @Environment(\.alertKey) private var alertManager
+    @Environment(\.progressKey) private var progressManager
 
     init(viewModel: LeaguesViewModel) {
         self._viewModel = State(initialValue: viewModel)
     }
 
     var body: some View {
-        List(viewModel.leagues) { league in
-            HStack(spacing: 20) {
-                AsyncImage(url: URL(string: league.league_logo ?? "")) { phase in
-                    switch phase {
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 70, height: 70)
-                            .roundedCornerWithBorder(lineWidth: 1, borderColor: .main, radius: 20, corners: .allCorners)
-                    default:
-                        Image(.logoOlympic)
-                            .resizable()
-                            .frame(width: 70, height: 70)
-                            .roundedCornerWithBorder(lineWidth: 1, borderColor: .main, radius: 20, corners: .allCorners)
+        LeaguesList()
+            .background(Color.background)
+            .searchable(text: $viewModel.searchText)
+            .onAppear {
+            self.viewModel.alertManager = alertManager
+            self.viewModel.progressManager = progressManager
+            self.viewModel.leagues.removeAll()
+        }
+            .task {
+            self.viewModel.fetchLeagues()
+        }
+            .refreshable {
+            self.viewModel.fetchLeagues(isShowLoader: false)
+        }
+    }
+}
+
+extension LeaguesView {
+    private func LeagueCell(_ league: League) -> some View {
+        return HStack(spacing: 20) {
+            ImageLoadingView(url: league.league_logo)
+                .frame(width: 70, height: 70)
+            Text(league.league_name ?? "")
+                .font(.custom("Poppins-Medium", size: 14))
+                .foregroundStyle(Color.foreground)
+            Spacer()
+        }
+            .padding(15)
+            .background(.secondaryBackground)
+            .cornerRadius(radius: 20)
+            .shadow(color: .shadow, radius: 5, x: 0, y: 2)
+    }
+
+    private func LeaguesList() -> some View {
+        return List {
+            Group {
+                if viewModel.leagues.isEmpty && !progressManager.showProgress {
+                    Spacer(minLength: (UIScreen.screenHeight / 2) - 150)
+                    ContentUnavailableView("No \(viewModel.sport.title) Leagues Available", systemImage: viewModel.sport.icon)
+                } else {
+                    ForEach(viewModel.leagues) { league in
+                        LeagueCell(league)
                     }
                 }
-                Text(league.league_name ?? "")
-                    .font(.custom("Poppins-Medium", size: 14))
-                    .foregroundStyle(Color.foreground)
-                Spacer()
             }
-                .padding(15)
-                .background(.secondaryBackground)
-                .listRowBackground(Color.background)
-                .cornerRadius(radius: 20)
-                .shadow(color: .shadow, radius: 5, x: 0, y: 2)
                 .listRowInsets(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+                .listRowBackground(Color.background)
                 .listRowSeparator(.hidden)
         }
             .listStyle(.plain)
@@ -59,5 +80,4 @@ struct LeaguesView: View {
     viewModel.leagues.append(.init(league_key: 3, league_name: "UEFA Champions League", country_key: 1, country_name: "Eurocups", league_logo: "https://apiv2.allsportsapi.com/logo/logo_leagues/3_uefa_champions_league.png",
         country_logo: nil))
     return LeaguesView(viewModel: viewModel)
-
 }
--- Sports/Views/Scenes/Leagues/ViewModel/LeaguesViewModel.swift
+++ Sports/Views/Scenes/Leagues/ViewModel/LeaguesViewModel.swift
@@ -9,12 +9,44 @@ import Foundation
 
 @Observable
 class LeaguesViewModel {
+    private var originalLeagues: [League] = []
+    var leagues: [League] = []
     let title = "Leagues"
     var sport: Sports
-    var leagues: [League] = []
+    var alertManager: AlertManager?
+    var progressManager: ProgressManager?
+    var searchText: String = "" {
+        didSet {
+            findLeagues()
+        }
+    }
 
     init(sport: Sports) {
         self.sport = sport
+    }
+
+    func fetchLeagues(isShowLoader: Bool = true) {
+        guard let alertManager, let progressManager else { return }
+        let baseRequest = BaseRequest()
+        baseRequest.end_point = sport.endPoint
+        baseRequest.method = .get
+        baseRequest.parameters = [
+            "met": APIConstants.API_leagues.rawValue,
+            "APIkey": APIConstants.API_key.rawValue
+        ]
+        _ = RequestBuilder(alertManager: alertManager, progressManager: progressManager).requestWithSuccessResponse(with: baseRequest, isShowLoader: isShowLoader, isShowMessage: true, success: { [weak self] response, code, message in
+            let result = response["result"] as? [[String: Any]]
+            self?.originalLeagues = League.modelsFromDictionaryArray(array: result)
+            self?.leagues = self?.originalLeagues ?? []
+        })
+    }
+
+    private func findLeagues() {
+        guard !searchText.isEmpty else {
+            leagues = originalLeagues
+            return
+        }
+        leagues = originalLeagues.filter { $0.league_name?.lowercased().contains(searchText.lowercased()) ?? false }
     }
 
 }
