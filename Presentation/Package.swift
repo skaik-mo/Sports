f5feaca74bd23a6557f4e3ff739e7007c9e7da6e
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2026-06-09 14:32:07 +0300

    downgrade swift-tools-version to 6.0 and update iOS platform version to 15 for Data, Domain, and Presentation packages

--- Presentation/Package.swift
+++ Presentation/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Presentation",
-    platforms: [.iOS(.v18)],
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
