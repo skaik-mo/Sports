4aca94767e60cf630fe01b2ab25965b1896b1fb7
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2026-07-02 00:01:50 +0300

    Rename AppIcon to AppIcons

--- DesignSystem/Sources/DesignSystem/Components/Icons/AppIcons.swift
+++ DesignSystem/Sources/DesignSystem/Components/Icons/AppIcons.swift
@@ -1,5 +1,5 @@
 //
-//  AppIcon.swift
+//  AppIcons.swift
 //  DesignSystem
 //
 //  Created by Mohammed Skaik on 29/06/2026.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum AppIcon {
+public enum AppIcons {
     public static let grid2x2 = "square.grid.2x2"
     public static let list1x2 = "rectangle.grid.1x2"
 }
